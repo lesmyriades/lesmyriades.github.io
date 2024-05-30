183da062752da71752ead22db795cca14953a383
Tassanee Alleau <talamani@proton.me>
2024-05-30 11:53:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Script pour zoomer et déplacer l'image en plein écran
-    const imageElement = document.querySelector('#full-screen-image');
-    const panzoom = Panzoom(imageElement, {
-        maxScale: 10,
-        minScale: 1,
-        contain: 'outside',
-        step: 0.1,
+    const images = document.querySelectorAll('.full-screen-image img');
+    images.forEach((imageElement) => {
+        const panzoom = Panzoom(imageElement, {
+            maxScale: 10,
+            minScale: 1,
+            contain: 'outside',
+            step: 0.1,
+        });
+        imageElement.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
     });
 
-    imageElement.parentElement.addEventListener('wheel', panzoom.zoomWithWheel);
-    
+    // Script pour ajouter du texte aléatoire à intervalles réguliers
+    const randomTextContainer = document.getElementById('randomTextContainer');
 
-const randomTextContainer = document.getElementById('randomTextContainer');
+    function generateRandomText() {
+        const words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Integer", "nec", "odio", "Praesent", "libero", "Sed", "cursus", "ante", "dapibus", "diam", "Sed", "nisi"];
+        const randomWord = words[Math.floor(Math.random() * words.length)];
+        return randomWord;
+    }
 
-function generateRandomText() {
-    const words = ["root", "root", "roots", "rooted", "uprooted", "unrooted", "rooting", "radix", "radices", "wurtz", "racine", "raciner", "enraciner"];
-    const randomWord = words[Math.floor(Math.random() * words.length)];
-    return randomWord;
-}
+    function addRandomText() {
+        const randomText = generateRandomText();
+        const textNode = document.createTextNode(randomText + " ");
+        randomTextContainer.appendChild(textNode);
+    }
 
-function addRandomText() {
-    const randomText = generateRandomText();
-    const textNode = document.createTextNode(randomText + " ");
-    randomTextContainer.appendChild(textNode);
-}
-
-setInterval(addRandomText, 1000); // Change 1000 to the desired interval in milliseconds
-
-
+    setInterval(addRandomText, 1000); // Change 1000 to the desired interval in milliseconds
+});
