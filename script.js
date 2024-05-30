43e3cae5dfe3d8d5f90fd52d84300291977812b2
Tassanee Alleau <talamani@proton.me>
2024-05-30 12:03:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     // Script pour ajouter du texte aléatoire à intervalles réguliers
     const randomTextContainer = document.getElementById('randomTextContainer');
+    
+    if (randomTextContainer) {
+        function generateRandomText() {
+            const words = ["roots", "root", "rooted"];
+            const randomWord = words[Math.floor(Math.random() * words.length)];
+            return randomWord;
+        }
 
-    function generateRandomText() {
-        const words = ["root", "roots"];
-        const randomWord = words[Math.floor(Math.random() * words.length)];
-        return randomWord;
+        function addRandomText() {
+            const randomText = generateRandomText();
+            const textNode = document.createTextNode(randomText + " ");
+            randomTextContainer.appendChild(textNode);
+        }
+
+        setInterval(addRandomText, 1000); // Change 1000 to the desired interval in milliseconds
+    } else {
+        console.error("Element #randomTextContainer not found.");
     }
-
-    function addRandomText() {
-        const randomText = generateRandomText();
-        const textNode = document.createTextNode(randomText + " ");
-        randomTextContainer.appendChild(textNode);
-    }
-
-    setInterval(addRandomText, 1000); // Change 1000 to the desired interval in milliseconds
 });
